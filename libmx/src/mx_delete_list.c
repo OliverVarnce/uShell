6342ca5bdee78556b09bc5e4346d50faa90d7e07
Oleh Voitenko <voytenko.ov@gmail.com>
2020-06-24 21:29:32 +0300

    bugfix

--- libmx/src/mx_delete_list.c
+++ libmx/src/mx_delete_list.c
@@ -4,17 +4,10 @@ void mx_del_list(t_list **lst) {
     t_list *w = *lst;
 
     while (w != NULL) {
-<<<<<<< HEAD
         t_list *tmp = w;
         
         w = w->next;
         free(tmp);
-=======
-        t_list *temp = w;
-        
-        w = w->next;
-        free(temp);
->>>>>>> master
     }
     *lst = NULL;
 }
